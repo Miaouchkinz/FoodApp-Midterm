b88515e87df976af71727ca572c2010b3008b363
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-08 13:14:46 -0500

    create meal_items table

---
+++ db/schema/03_menu_items.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS meal_items CASCADE;
+
+CREATE TABLE meal_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  ingredient_list TEXT NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  allergens TEXT,
+  meal_category_id INTEGER REFERENCES meal_categories(id) ON DELETE CASCADE
+);
