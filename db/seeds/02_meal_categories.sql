a6d3df06deb9a95b3a5ed656a9543bc9d8421f5f
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-09 15:59:42 -0500

    seed meal_categories table

---
+++ db/seeds/02_meal_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO meal_categories (name, description)
+VALUES ('sandwich', 'reliable and easy to carry meal between two pieces of bread.'),
+('soup', 'a warm bowl of delicious goodness.'),
+('salad', 'a healthy meal filled with mostly vegetables.'),
+('drinks', 'beverages to quench your thirst.');
