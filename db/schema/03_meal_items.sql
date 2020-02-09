1e568c1a10f2edb8514c42d6663e3aaf1fe6c9e2
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-09 15:57:49 -0500

    rename file and allow description to be empty

--- db/schema/03_meal_items.sql
+++ db/schema/03_meal_items.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS meal_items CASCADE;
 CREATE TABLE meal_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT,
   ingredient_list TEXT NOT NULL,
   price INTEGER NOT NULL DEFAULT 0,
   allergens TEXT,
