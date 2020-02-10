52eedebae6bbf31bd937e0c1173b81b80ea6b6d2
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-10 13:20:36 -0500

    add menuRoutes

---
+++ routes/menu.js
@@ -0,0 +1,31 @@
+/*
+ * All routes for Menu are defined here
+ * Since this file is loaded in server.js into api/menu,
+ *   these routes are mounted onto /menu
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const dbHelpers = require('./dbHelpers');
+
+const menuRoutes = (db) => {
+  const {getMenuItemsByCategory} = dbHelpers(db);
+
+  router.get("/:category", (req, res) => {
+    const category = req.params.category;
+    getMenuItemsByCategory(category)
+      .then( (menuItems) => {
+        res.status(201).json(menuItems.rows );
+      })
+      .catch(err => {
+        res
+          .status(500) // TOFIX: check to see what status code to use here.
+          .json( {error: err.message})
+      });
+  });
+
+  return router;
+};
+
+module.exports = menuRoutes
