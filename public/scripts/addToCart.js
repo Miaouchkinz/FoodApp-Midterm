11096ace76f0bea0a1ec447a946498e1d11399c8
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-11 08:57:51 -0500

    move event handler to menuCategory and removed from doc ready

--- public/scripts/addToCart.js
+++ public/scripts/addToCart.js
@@ -1,24 +1,20 @@
-$(document).ready(() => {
+const addItemToCart = (id, mealItemsArray) => {
+  const mealItem = mealItemsArray.find(item => item['id'].toString() === id);
+  let checkItemExist = JSON.parse(localStorage.getItem(JSON.stringify(id)));
 
-  const addItemToCart = (id, mealItemsArray) => {
-    const mealItem = mealItemsArray.find(item => item['id'].toString() === id);
-    let checkItemExist = JSON.parse(localStorage.getItem(JSON.stringify(id)));
-
-    if (!checkItemExist) {
-      localStorage.setItem(JSON.stringify(id), JSON.stringify({...mealItem, qty: 1}));
-    } else {
-      checkItemExist.qty += 1;
-      localStorage.setItem(JSON.stringify(id), JSON.stringify(checkItemExist));
-    }
-
+  if (!checkItemExist) {
+    localStorage.setItem(JSON.stringify(id), JSON.stringify({...mealItem, qty: 1}));
+  } else {
+    checkItemExist.qty += 1;
+    localStorage.setItem(JSON.stringify(id), JSON.stringify(checkItemExist));
   }
+}
 
-  $(".add-to-cart").click((e) => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/menu/'
-    })
-    .then((res) => { addItemToCart(e.target.id, res)})
-  });
-
-});
+const attachCartHandler = (e) => {
+  console.log(e.target);
+  $.ajax({
+    method: 'GET',
+    url: '/api/menu/'
+  })
+  .then((res) => { addItemToCart(e.target.id, res)})
+}
