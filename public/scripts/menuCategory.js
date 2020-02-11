825325a3002477416b22ce73e1ff22f55be1ed64
yoon soon kang <kangyoonsoon@hotmail.com>
2020-02-11 00:49:10 +0000

    hide place order contanier when webpage load

--- public/scripts/menuCategory.js
+++ public/scripts/menuCategory.js
@@ -1,111 +1,61 @@
 $(document).ready(function () {
-//   const mealItem = `<div class="meal-items conainer-lg">
-//   <div class="row meal-items-row">
-//     <div class="col-lg-3">
-//       <img src="/images/sandwich7.png" alt="BLT">
-//     </div>
+  $('.place-order').hide();
 
-//     <div class="col-lg-9 meal-items-row-container">
-//         <div class="row meal-items-row">
-//             <div class="col-lg-8 meal-items-row-description">
-//                 <span class="heading">
-//                     <p>
-//                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis ratione unde voluptatibus sapiente deserunt voluptate culpa nam non suscipit architecto, eos ullam perspiciatis, quasi laboriosam tenetur neque facere aliquam.
-//                     </p>
-//                 </span>
-//             </div>
-//             <div class="col-lg-1">
-//             </div>
-//             <div class="col-lg-3 price-quantity">
-//               <span>
-//                 <p>
-//                     <div class="btn-group" role="group" aria-label="Basic example">
-//                         <button type="button" class="btn btn-success">+</button>
-//                         <button type="button" class="btn btn-light">0</button>
-//                         <button type="button" class="btn btn-danger">-</button>
-//                       </div>
-//                 </p>
-//                 <p>
-//                   $ 0.00
-//                 </p>
-//               </span>
-//             </div>
-//         </div>
-//     </div>
-//     </div>
-//   </div>
-// </div>
-// `;
+  // New tweet HTML format
+  const createMenuItems = function(mealItem) {
+    console.log(mealItem);
+    const mealPrice = (mealItem.price / 100).toFixed(2);
+    const $menuElement = `<div class="meal-items conainer-lg">
+    <div class="row meal-items-row">
+      <div class="col-lg-3 my-auto">
+        <h3>${mealItem.name}</h3>
+      </div>
 
-
-  // $('#sandwich').click(function(){
-
-  //   $.ajax('/scripts/test1.js', { method: 'GET' })
-  //   .then(function (mealItem) {
-  //     console.log('Success: ', mealItem);
-  //     $('#meal-items').prepend(mealItem);
-  //   });
-
-  // });
-
-    // New tweet HTML format
-    const createMenuItems = function(mealItem) {
-      console.log(mealItem);
-      const mealPrice = (mealItem.price / 100).toFixed(2);
-      const $menuElement = `<div class="meal-items conainer-lg">
-      <div class="row meal-items-row">
-        <div class="col-lg-3 my-auto">
-          <h3>${mealItem.name}</h3>
-        </div>
-
-        <div class="col-lg-9 meal-items-row-container">
-            <div class="row meal-items-row">
-                <div class="col-lg-8 meal-items-row-description">
-                    <span class="heading">
-                        <p>
-                        ${mealItem.description}
-                        </p>
-                        <p><i>
-                        Ingredients: ${mealItem.ingredient_list}
-                        </i></p>
-                    </span>
-                </div>
-                <div class="col-lg-1">
-                </div>
-                <div class="col-lg-3 price-quantity my-auto">
-                  <span>
-                    <p>
-                      <div class="btn-group" role="group" aria-label="Basic example">
-                          <button type="button" class="btn btn-success">-</button>
-                          <button type="button" class="btn btn-light">0</button>
-                          <button type="button" class="btn btn-danger">+</button>
-                      </div>
-                    </p>
-                    <p>
-                     $ ${mealPrice}
-                    </p>
+      <div class="col-lg-9 meal-items-row-container">
+          <div class="row meal-items-row">
+              <div class="col-lg-8 meal-items-row-description">
+                  <span class="heading">
+                      <p>
+                      ${mealItem.description}
+                      </p>
+                      <p><i>
+                      Ingredients: ${mealItem.ingredient_list}
+                      </i></p>
                   </span>
-                </div>
-            </div>
-        </div>
-        </div>
+              </div>
+              <div class="col-lg-1">
+              </div>
+              <div class="col-lg-3 price-quantity my-auto">
+                <span>
+                  <p>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" id="${mealItem.id}" class="meal_item_${mealItem.id} btn btn-warning btn-lg">Place to order</button>
+
+                    </div>
+                  </p>
+                  <p>
+                    $ ${mealPrice}
+                  </p>
+                </span>
+              </div>
+          </div>
+      </div>
       </div>
     </div>
-    `;
+  </div>
+  `;
 
-      return $menuElement;
-    };
+    return $menuElement;
+  };
 
 
 
   // Rendering taken from [{}] Json format
   const renderMenuElements = function (menuItemArray) {
-    console.log(menuItemArray);
+
     $menuItems = '';
     for (let i = 0; i < menuItemArray.length; i++) {
-      let lastMenuItem = menuItemArray.shift();
-      $menuItems += createMenuItems(lastMenuItem);
-      console.log('lastItems:   ',lastMenuItem);
+      $menuItems += createMenuItems(menuItemArray[i]);
     }
     $('#meal-items').prepend($menuItems);
   };
@@ -140,56 +90,4 @@ $(document).ready(function () {
 
 });
 
-  // $button.on('click', function () {
-  // $.ajax('test1.html', { method: 'GET' })
-  //   .then(function (testHTML) {
-  //     console.log('Success: ', testHTML);
-  //     $button.prepend(testHTML);
-  //   });
-  // });
 
-
-  // Create new meal item elements HTML format
-  // const createMealItemElements = function(tweet) {
-  //   const userTweet = `${escape(tweet.content.text)}`;
-  //   let $tweet = `<article class="tweet-container">
-  //     <div class="tweet-container-header">
-  //       <div style="flex-grow: 1;">
-  //           <img class="newton" src="${tweet.user.avatars}">
-  //       </div>
-  //       <div style="flex-grow: 1;">
-  //           <h2>${tweet.user.name}</h2>
-  //       </div>
-  //       <div style="flex-grow: 8;"></div>
-  //       <div style="flex-grow: 1;">
-  //           <h2>${tweet.user.handle}</h2>
-  //       </div>
-  //     </div>
-  //     <div class="tweet-container-content">
-  //       <h2>${userTweet}</h2>
-  //     </div>
-  //     <footer class="formButtonCounter">
-  //       <span>${tweet.created_at}</span>
-  //       <span class="icons">
-  //       <img src="/images/flag.png">
-  //       <img src="/images/retweet.png">
-  //       <img src="/images/heart.png">
-  //       </span>
-  //     </footer>
-  //   </article>`;
-
-  //   return $tweet;
-  // };
-
-
-// $(function() {
-//   const $button = $('#load-more-posts');
-//   $button.on('click', function () {
-//     console.log('Button clicked, performing ajax call...');
-//     $.ajax('more-posts.html', { method: 'GET' })
-//     .then(function (morePostsHtml) {
-//       console.log('Success: ', morePostsHtml);
-//       $button.replaceWith(morePostsHtml);
-//     });
-//   });
-// });
