82783e12c2a29c18e80502d21167518eda4cce60
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-11 08:58:53 -0500

    remove test logs, update add-to cart class, add event handler

--- public/scripts/menuCategory.js
+++ public/scripts/menuCategory.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 
   // New tweet HTML format
   const createMenuItems = function(mealItem) {
-    console.log(mealItem);
     const mealPrice = (mealItem.price / 100).toFixed(2);
     const $menuElement = `<div class="meal-items conainer-lg">
     <div class="row meal-items-row">
@@ -29,7 +28,7 @@ $(document).ready(function () {
                 <span>
                   <p>
                     <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" id="${mealItem.id}" class="meal_item_${mealItem.id} btn btn-warning btn-lg">Place to order</button>
+                        <button type="button" id="${mealItem.id}" class="add-to-cart btn btn-warning btn-lg">Place to order</button>
 
                     </div>
                   </p>
@@ -58,6 +57,7 @@ $(document).ready(function () {
       $menuItems += createMenuItems(menuItemArray[i]);
     }
     $('#meal-items').prepend($menuItems);
+    $(".add-to-cart").click(attachCartHandler);
   };
 
   const loadMenuSandwitch = function (menuItem) {
