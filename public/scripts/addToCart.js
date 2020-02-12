7d75127a15a56101a7a90dd3d27bbb794b84e467
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-11 22:14:43 -0500

    add update cart item qty and price / total

--- public/scripts/addToCart.js
+++ public/scripts/addToCart.js
@@ -8,12 +8,18 @@ const addItemToCart = (id, mealItemsArray) => {
     checkAddedItem.qty += 1;
     localStorage.setItem(JSON.stringify(id), JSON.stringify(checkAddedItem));
   }
+}
 
-  return cartCounter();
+const removeItemFromCart = (id, mealItemsArray) => {
+  const mealItem = mealItemsArray.find(item => item['id'].toString() === id);
+  let checkAddedItem = JSON.parse(localStorage.getItem(JSON.stringify(id)));
+
+  checkAddedItem.qty -= 1;
+  localStorage.setItem(JSON.stringify(id), JSON.stringify(checkAddedItem));
 }
 
 // Add to counter as items are added to cart
-const cartCounter = () => {
+const updateCartCounter = () => {
   let sum = 0;
   for (let i =0; i < localStorage.length; i++) {
     let mealItem = JSON.parse(localStorage.getItem(localStorage.key(i)));
@@ -22,10 +28,68 @@ const cartCounter = () => {
   $('#cart-counter').text(0 + sum);
 }
 
-const attachCartHandler = (e) => {
+const updateQuantityCounter = (id, increase) => {
+  let qtyCounter = $('#qty-' + id);
+  let quantity = Number(qtyCounter.text());
+  if (increase) quantity++;
+  else quantity--;
+
+  qtyCounter.text(quantity);
+}
+
+const updateItemTotal = (id, price) => {
+  let itemPrice = $('#meal-item-' + id + ' .item-price');
+  let itemQty = Number($('#qty-' + id).text());
+  let newPrice = price * itemQty;
+
+  itemPrice.text(newPrice.toFixed(2));
+}
+
+const calculateSubTotal = () => {
+  let sum = 0;
+  $('.item-price').each((index, item) => {
+    sum += Number($(item).text());
+  });
+  $('.sub-total-cost').text(sum.toFixed(2));
+};
+
+const calculateTip = (percentage) => {
+  let tipValue = percentage * Number($('.sub-total-cost').text());
+  $('.total-tip').text(tipValue.toFixed(2));
+  calculateTotalCost();
+}
+
+const calculateTotalCost = () => {
+  let totalCost = Number($('.sub-total-cost').text()) + Number($('.total-tip').text());
+  $('.total-cost').text(totalCost.toFixed(2));
+}
+
+const addToCartHandler = (e) => {
   $.ajax({
     method: 'GET',
     url: '/api/menu/'
   })
-  .then((res) => { addItemToCart(e.target.id, res)})
+  .then((res) => {
+    addItemToCart(e.target.id, res);
+    updateCartCounter();
+    updateQuantityCounter(e.target.id, true);
+    updateItemTotal(e.target.id, res[e.target.id - 1].price / 100)
+    calculateSubTotal();
+    calculateTotalCost();
+  })
+}
+
+const removeFromCartHandler = (e) => {
+  $.ajax({
+    method: 'GET',
+    url: '/api/menu/'
+  })
+  .then((res) => {
+    removeItemFromCart(e.target.id, res);
+    updateCartCounter();
+    updateQuantityCounter(e.target.id, false);
+    updateItemTotal(e.target.id, res[e.target.id - 1].price / 100)
+    calculateSubTotal();
+    calculateTotalCost();
+  })
 }
