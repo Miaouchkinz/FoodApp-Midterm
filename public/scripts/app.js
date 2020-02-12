b1454ec6d5f5c4782ae487389352e0da907aa09a
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-11 22:20:26 -0500

    add on page load functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,19 @@
+const hideMenuPage = () => {
+  $('.meal-items').remove();
+  $('.menu-category').hide();
+  $('.place-order').show();
+}
+
 $(() => {
   // Ensures the cart count is still there on page load
-  cartCounter();
+  updateCartCounter();
+  createTipHandlers();
+
+  $('.cart > img').click(() => {
+    hideMenuPage();
+    renderCartElements();
+
+    calculateSubTotal();
+    calculateTotalCost();
+  });
 });
