d6ade726fe43d59e9d15a918e9de7a0a9176f106
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-12 17:13:05 -0500

    set some of entities to completed

--- db/seeds/04_orders.sql
+++ db/seeds/04_orders.sql
@@ -1,6 +1,6 @@
-INSERT INTO orders (client_id)
-VALUES (1),
-(2),
-(3),
-(2),
-(3);
+INSERT INTO orders (client_id, is_complete)
+VALUES (1, true),
+(2, false),
+(3, false),
+(2, true),
+(3, false);
