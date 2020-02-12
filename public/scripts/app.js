868e47544b985c7b9ab0323485f7013c39fc3149
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-11 22:20:47 -0500

    move hidemenu and checkout handler to cartItems

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,5 @@
-const hideMenuPage = () => {
-  $('.meal-items').remove();
-  $('.menu-category').hide();
-  $('.place-order').show();
-}
-
 $(() => {
-  // Ensures the cart count is still there on page load
   updateCartCounter();
   createTipHandlers();
-
-  $('.cart > img').click(() => {
-    hideMenuPage();
-    renderCartElements();
-
-    calculateSubTotal();
-    calculateTotalCost();
-  });
+  openCheckoutHandler();
 });
