7cee5f6aee38e1927676843ff10b5ab9cdae621c
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-12 20:12:49 -0500

    create orders route mount

--- server.js
+++ server.js
@@ -46,12 +46,14 @@ app.use(cookieSession({
 const usersRoutes = require("./routes/users");
 const menuRoutes = require("./routes/menu");
 const adminRoutes = require("./routes/admin");
+const orderRoutes = require("./routes/orders");
 const widgetsRoutes = require("./routes/widgets");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/menu", menuRoutes(db));
 app.use("/api/admin", adminRoutes(db));
+app.use("/api/orders", orderRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
