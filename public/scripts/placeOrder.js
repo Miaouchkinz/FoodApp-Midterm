788e834cba4a3fc3ba4a87f298f6c172a63750d0
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-13 15:12:00 -0500

    update getCartItemAndQty, create event handler and ajax post

--- public/scripts/placeOrder.js
+++ public/scripts/placeOrder.js
@@ -1,37 +1,35 @@
 $(document).ready(function () {
 
-// create function that grabs values of keys in
-// the object that is the value of the key in local storage
-// store as meal_item_id: qty
+
   const getCartItemAndQty = () => {
-    let cart = []
+    let cartItems = []
     let keys = Object.keys(localStorage);
 
     for (let [key, value] of Object.entries(localStorage)) {
-      cart.push(JSON.parse(localStorage.getItem(key)))
+      cartItems.push(JSON.parse(localStorage.getItem(key)))
     }
 
-    return cart.map((x, i) => { return { [keys[i]] : x.qty } });
+    cartItems.map((x, i) => { return { [keys[i]] : x.qty } });
+
+    return cartItems;
   }
-
+  // create event.handler that triggers above
   // pass info to ajax post to create order
-
-
+  // once all is done, it should clear cart and render waiting page
+  $('#place-order').click( () => {
+    // getCartItemAndQty()
+    //   .then( (cartItems) => {
+        $.ajax({
+          method: 'POST',
+          url: '/api/orders/new',
+          data: {
+            cartItems: getCartItemAndQty()
+          }
+        })
+      .then(() => {
+        localStorage.clear();
+      })
+      //TO ADD: .then(render waiting page function)
+  })
 
 });
-
-//============
-
-// pass object to ajax post to create order
-
-// ==========
-
-// create event.handler that triggers above ^^
-// once all is done, it should clear cart and render waiting page
-
-//============
-
-// create backend route for post
-// create DB query to add entry to orders table and order_items table
-
-//
