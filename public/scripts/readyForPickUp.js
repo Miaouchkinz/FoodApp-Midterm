b22eef732a24d5880bd0d4b4cc15d1993b82c385
yoon soon kang <kangyoonsoon@hotmail.com>
2020-02-13 21:14:12 +0000

    The waiting box show up when pushing place order

--- public/scripts/readyForPickUp.js
+++ public/scripts/readyForPickUp.js
@@ -20,6 +20,37 @@ $(document).ready(function () {
 
     // Notify client to order number in waiting
     const notifyClientWaiting= `
+    <section class="waiting">
+    <div class="conainer-lg">
+    <div class="row waiting-row">
+      <div class="col">
+        <img src="/images/step-bar5.png" alt="step-bar">
+      </div>
+      <div class="w-100"></div>
+      <div class="col-lg-1 my-auto"></div>
+      <div class="col-lg-1 my-auto"></div>
+
+      <div class="col-lg-2 my-auto">Choose your foods</div>
+      <div class="col-lg-1 my-auto"></div>
+
+      <div class="col-lg-2 my-auto">Order complete</div>
+      <div class="col-lg-1 my-auto"></div>
+      <div class="col-lg-2 my-auto">Ready for pickup</div>
+
+      <div class="col-lg-1 my-auto"></div>
+      <div class="col-lg-1 my-auto"></div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <div class="conainer-lg">
+    <div class="row waiting-row">
+      <div class="col-lg-3 my-auto"></div>
+      <div class="col-lg-6 my-auto">
+        <img src="/images/Catering3.png" alt="step-bar">
+        <div id="waitingOrderNumber"></div>
+
     <h3>Order number: ${orderNumber}</h3>
     <h3>Name: ${name}</h3>
     <p>
@@ -28,9 +59,15 @@ $(document).ready(function () {
   </p>
   <p>
     We will notify you via SMS message when it's ready to pick up
-  </p>`;
+  </p>
+  </div>
+  <div class="col-lg-3 my-auto"></div>
+</div>
+</div>
+</section>`;
 
-    $('#waitingOrderNumber').append(notifyClientWaiting);
+    // $('#waitingOrderNumber').append(notifyClientWaiting);
+    $('#waiting').append(notifyClientWaiting);
     $(this).closest( ".upcomming-order" ).hide();
 
   });
