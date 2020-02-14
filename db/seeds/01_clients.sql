ed2043fac10be3b9cdd686020812a85d78378246
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-14 16:11:34 -0500

    update seed comment to use clients instead of user

--- db/seeds/01_clients.sql
+++ db/seeds/01_clients.sql
@@ -1,4 +1,4 @@
--- Users table seeds here (Example)
+-- Clients table seeds here (Example)
 INSERT INTO clients (name, email, phone_number)
 VALUES ('Fred', 'fred@gmail.com', '+15145925988'),
 ('Gab', 'g.elodierichard@gmail.com', '+15148240269'),
