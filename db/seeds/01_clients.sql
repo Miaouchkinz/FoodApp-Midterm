fcffc38157cd1953a11346186e42d018c9163bc4
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-13 22:49:25 -0500

    update phone number format

--- db/seeds/01_clients.sql
+++ db/seeds/01_clients.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO clients (name, email, phone_number)
-VALUES ('Fred', 'fred@gmail.com', '1-514-592-5988'),
-('Gab', 'g.elodierichard@gmail.com', '1-514-824-0269'),
-('Yoon Soon', 'yoonsoon@gmail.com', '1-514-431-4459');
+VALUES ('Fred', 'fred@gmail.com', '+15145925988'),
+('Gab', 'g.elodierichard@gmail.com', '+15148240269'),
+('Yoon Soon', 'yoonsoon@gmail.com', '+15144314459');
 
